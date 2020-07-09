6028d1e5b80e0daf4be3ce9e1fc41c09afe3a723
Guillermo Padilla <gcpadilla@gmail.com>
2020-07-09 15:16:25 -0300

    delete-adit-postulates

--- backend/src/controllers/postulateController.js
+++ backend/src/controllers/postulateController.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator");
 const postulateModel = require("../models/postulateModel");
 const mongoose = require("mongoose");
 const candidateModel = require("../models/candidateModel");
-const offerModel = require("../models/offerModel")
+const offerModel = require("../models/offerModel");
 
 //crear postulacion
 exports.createPostulate = async (req, res) => {
@@ -31,13 +31,17 @@ exports.createPostulate = async (req, res) => {
     if (candidate_id) {
       await postulate.save();
 
-      const offer = await offerModel.findById({_id: mongoose.Types.ObjectId(req.params.offerId)});
-      const candidate = await candidateModel.findById({_id: res.locals.user.id});
+      const offer = await offerModel.findById({
+        _id: mongoose.Types.ObjectId(req.params.offerId),
+      });
+      const candidate = await candidateModel.findById({
+        _id: res.locals.user.id,
+      });
 
-      offer.postulateRef.push(postulate._id)
-      await offer.save()
-      candidate.postulateRef.push(postulate._id)
-      await candidate.save()
+      offer.postulateRef.push(postulate._id);
+      await offer.save();
+      candidate.postulateRef.push(postulate._id);
+      await candidate.save();
 
       res.send({
         message: "Se registro postulacion correctamente..",
@@ -62,8 +66,11 @@ exports.getAllPostulates = async (req, res) => {
 //todas las postulaciones user
 exports.getAllPostulatessUser = async (req, res) => {
   try {
-    const postulates = await postulateModel.find({candidateid: res.locals.user.id}, "-candidateid");
-    console.log(postulates)
+    const postulates = await postulateModel.find(
+      { candidateid: res.locals.user.id },
+      "-candidateid"
+    );
+    console.log(postulates);
     res.send(postulates);
   } catch (err) {
     res.status(500).send(err);
@@ -72,12 +79,16 @@ exports.getAllPostulatessUser = async (req, res) => {
 
 //Editar postulación
 exports.updatePostulate = async (req, res) => {
-	try {
-		if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-			return res.status(404).json({ message: "No de encontro postulación ..." });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res
+        .status(404)
+        .json({ message: "No de encontro postulación ..." });
     }
-    
-    const postulate_in_db = await postulateModel.findOne({ _id: req.params.id });
+
+    const postulate_in_db = await postulateModel.findOne({
+      _id: req.params.id,
+    });
 
     if (!postulate_in_db) {
       return res.status(400).json({ message: "Credenciales no validas." });
@@ -85,43 +96,52 @@ exports.updatePostulate = async (req, res) => {
 
     const { body } = req;
 
-  const postulateData = {
-    state: postulate_in_db.state,
-    intendedsalary: body.intendedsalary,
-    experiences: body.experiences,
-    studies: body.studies,
-    emailcandidate: body.emailcandidate,
-  };
+    const postulateData = {
+      state: postulate_in_db.state,
+      intendedsalary: body.intendedsalary,
+      experiences: body.experiences,
+      studies: body.studies,
+      emailcandidate: body.emailcandidate,
+    };
 
-		let postulate = await postulateModel.findByIdAndUpdate(
-			req.params.id,
-			postulateData,
-			{ new: true }
+    let postulate = await postulateModel.findByIdAndUpdate(
+      req.params.id,
+      postulateData,
+      { new: true }
     );
-    
-    postulate = await postulateModel.findOne({ _id: req.params.id }, "-candidateid");
 
-		if (!postulate) {
-			return res.status(404).json({ message: "No de encontro postulación ..." });
-		}
+    postulate = await postulateModel.findOne(
+      { _id: req.params.id },
+      "-candidateid"
+    );
 
-		res.send(postulate);
-	} catch (err) {
-		res.status(500).send({ message: "Error al editar postulación ..." });
-	}
+    if (!postulate) {
+      return res
+        .status(404)
+        .json({ message: "No de encontro postulación ..." });
+    }
+
+    res.send(postulate);
+  } catch (err) {
+    res.status(500).send({ message: "Error al editar postulación ..." });
+  }
 };
 
 //Borrar postulacion
 exports.deletePostulate = async (req, res) => {
   try {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(404).json({ message: "No de encontro postulación ..." });
+      return res
+        .status(404)
+        .json({ message: "No de encontro postulación ..." });
     }
 
     const postulate = await postulateModel.findByIdAndDelete(req.params.id);
 
     if (!postulate) {
-      return res.status(404).json({ message: "No de encontro postulación ..." });
+      return res
+        .status(404)
+        .json({ message: "No de encontro postulación ..." });
     }
 
     return res.status(200).send({ message: "Postulación eliminada" });
