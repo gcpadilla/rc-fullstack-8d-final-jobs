81703e87d8e4f6f5e0357daa9a825abe978c7ea4
Your Namefredykv <fredykv@hotmail.com>
2020-07-16 18:45:19 -0300

    minor change

--- frontend/src/pages/company2.jsx
+++ frontend/src/pages/company2.jsx
@@ -1,233 +1,85 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Footer from "../components/Footer";
+import axios from "axios";
+import CardOfferts from "../components/CardOfferts";
+import FormJobPostulate from "../components/FormJobPostulate";
+import EditOffers from "../components/EditOffers"
+import { Link } from 'react-router-dom'
+import auth from "../utils/auth";
+import sweetalert from "sweetalert2";
+import { useHistory } from "react-router-dom";
+import logo from "../images/RollingJobs.svg";
+import profilePH from "../images/profile.jpg"
 
-import React from "react";
-// import moment from "moment";
-// import axios from "axios";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-// import "../App.css";
-// import Swal from "sweetalert2";
-// import { Redirect } from "react-router-dom";
-import { useState } from "react";
-
-const Register = () => {
-  const [UserSelec, setUserSelec] = useState({});
-//   const [redirec, setredirec] = useState(false);
-
-  const onsubmit = async (e) => {
-//     e.preventDefault();
-//     const anio = edad(UserSelec.age);
-//     UserSelec.age = anio;
-//     console.log("Pasó");
-
-//     if (UserSelec.password === UserSelec.password2) {
-//       try {
-//         await axios.post(
-//           "http://localhost:3001/api/v1/users/candidates",
-//           UserSelec
-//         );
-//         // setTimeout(() => {
-//         //   setredirec(true);
-//         // }, 1000);
-
-//         await Swal.fire("genial", "se registro sactifactoriamente!", "success");
-//         setredirec(true);
-//       } catch (err) {
-//         if (err.response.data.message === undefined) {
-//           Swal.fire(
-//             `Error de ${err.response.data.errors[0].param}`,
-//             err.response.data.errors[0].msg,
-//             "error"
-//           );
-//         } else {
-//           Swal.fire("Oops..", err.response.data.message, "error");
-//         }
-//       }
-//     } else if (UserSelec.password !== UserSelec.password2) {
-//       Swal.fire("Error de Password", "las contraseñas no son iguales", "error");
-//     }
-//   };
-//   const edad = (a) => {
-//     const nacimiento = moment(a);
-//     const hoy = moment();
-//     const anios = hoy.diff(nacimiento, "years");
-
-//     return anios;
-  };
-
-  const onInputChange = (e) => {
-    setUserSelec({
-      ...UserSelec,
-      [e.target.name]: e.target.value,
-      publicationdate: new Date().toLocaleString(),
-    });
-  };
-
-  return (
-    <div>
+const CandidateProfile = () => {
+    return (
+        <div className=" companyStyle container-fluid">
+        <div className="row">
+          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-inline sidebar collapse sidebarMenu sticky-top ">
   
-      <div className="container">
-        <div className="text-center pb-5 form-group mb-3">
-          <h3 className="mt-4 titulos">Bienvenido</h3>
-          <h5 className="mb-4 texdo">Registro de candidatos</h5>
-          <p className="mb-4 textNews">
-            {" "}
-            Por favor, ingrese sus datos personales para iniciar tu proceso a tu
-            nuevo trabajo.
-          </p>
-          <div className="mb-4">
-            <form onSubmit={onsubmit}>
-              <div className="form-group">
-                <div className=" form-row ">
-                  <div className="form-group col-6">
-                    <label htmlFor="Name">Nombre</label>
-                    <input
-                      type="text"
-                      required
-                      className="form-control "
-                      name="firstname"
-                      placeholder="Nombre"
-                      onChange={onInputChange}
-                    />
-                  </div>
+        <Link to="/">
+          <img src={logo} loading="lazy" className="logoStyle mb-3" />
+        </Link>
+  
+        <div className="sidebar-sticky d-flex flex-column justify-content-around mb-3">
+            <h2 className="textAdmin text-dark">Bienvenido usuario</h2>
 
-                  <div className="form-group col-6">
-                    <label htmlFor="lastname">Apellido</label>
-                    <input
-                      type="text"
-                      required
-                      className="form-control"
-                      name="lastname"
-                      placeholder="Apellido"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                </div>
-
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="dni">Documento</label>
-                    <input
-                      type="number"
-                      required
-                      className="form-control"
-                      name="dni"
-                      placeholder="DNI / Cedula de Identidad / Pasaporte"
-                      onChange={onInputChange}
-                    />
-                  </div>
-
-                  <div className="form-group col-md-6">
-                    <label htmlFor="Age">Fecha de Nacimiento</label>
-                    <input
-                      type="date"
-                      required
-                      className="form-control"
-                      name="age"
-                      placeholder="Fecha de Nacimiento"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                </div>
-
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="profession">Profesión Principal</label>
-                    <input
-                      type="text"
-                      required
-                      className="form-control"
-                      name="profession"
-                      placeholder="Profesión"
-                      onChange={onInputChange}
-                    />
-                  </div>
-
-                  <div className="form-group col-md-6">
-                    <label htmlFor="exampleInputEmail1">
-                      Correo Elctrónico
-                    </label>
-                    <input
-                      type="email"
-                      required
-                      className="form-control"
-                      name="email"
-                      aria-describedby="emailHelp"
-                      placeholder="Correo Elctrónico"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                </div>
-
-                <div className="form-row">
-                  <div className="form-group col-md-6">
-                    <label htmlFor="username">Nombre de Usuario</label>
-                    <input
-                      type="text"
-                      required
-                      className="form-control"
-                      name="username"
-                      placeholder="Nombre de Usuario"
-                      onChange={onInputChange}
-                    />
-                  </div>
-
-                  <div className="form-group col-md-3">
-                    {" "}
-                    <label htmlFor="inputPassword5">Contraseña</label>
-                    <input
-                      type="password"
-                      required
-                      placeholder="Contraseña"
-                      id="inputPassword5"
-                      name="password"
-                      className="form-control"
-                      aria-describedby="passwordHelpBlock"
-                      onChange={onInputChange}
-                    />
-                    <small
-                      id="passwordHelpBlock"
-                      className="form-text text-muted"
-                    >
-                      la password debe tener entre 8 y 20 caracteres, contener
-                      letras y numeros, tiene que contener por lo menos una
-                      mayuscula y una minuscula.
-                    </small>
-                  </div>
-                  <div className="form-group col-md-3">
-                    <label htmlFor="exampleInputPassword1">
-                      Repita la Contraseña
-                    </label>
-                    <input
-                      type="password"
-                      required
-                      className="form-control"
-                      name="password2"
-                      placeholder="Contraseña"
-                      onChange={onInputChange}
-                    />
-                  </div>
-                </div>
-              </div>
-              <button
-               
-                className="btn btn-danger rounded-pill mr-5"
-              >
-                Cancelar
-              </button>
-              <button
-                type="submit"
-                onSubmit={onsubmit}
-                className="btn btn-success rounded-pill"
-              >
-                Confirmar Registro
-              </button>
-            </form>
-          </div>
+             <img src={profilePH} className="profilePH img-fluid mx-auto d-block rounded-circle" />
+  
+            <ul className="nav flex-column d-flex mt-5">
+            <li className="nav-item">
+                <Link  type="submit"  className="text-dark"> Modificar Perfil</Link>
+            </li>
+            <li className="nav-item">
+                <Link  type="submit"  className="text-dark"> Ver Postulaciones</Link>
+            </li>
+            <li className="nav-item">
+                <Link  type="submit"  className="text-dark"> Ofertas Publicadas </Link>
+            </li>
+            </ul>            
+            <ul className="nav flex-column d-flex mt-5">
+                        <li className="nav-item">
+                <Link className="mt-auto" type="submit"  className="text-dark"> Cerrar Sesión</Link>
+            </li>
+            </ul>
+  
         </div>
-      </div>
-
-    </div>
-  );
+      </nav>
+  
+        <div className=" col-md-9 col-lg-10 companyData d-flex flex-column flex-wrap">
+          <div className="">
+  
+          </div>
+          <div className="">
+            {/* {publicar ? ( */}
+              <div></div>
+             
+            {<div></div> ? <div></div> : <div>
+              <h3 className="titulos text-center my-3">Ofertas Publicadas</h3>
+              <div className="d-flex flex-wrap justify-content-center"> 
+              {/* {cards} */}
+              </div> 
+              </div>}
+  
+  
+            {<div></div> ? (
+              <div></div>
+            ) : (
+              <div>
+                {/* <EditOffers oferta={id} terminar={mostrarcard}/> */}
+              </div>
+            )}
+  
+          </div>
+  
+  
+          </div>
+          </div>
+  
+  
+        {/* <Footer /> */}
+        </div>
+    );
 };
 
-export default Register;
+export default CandidateProfile;
