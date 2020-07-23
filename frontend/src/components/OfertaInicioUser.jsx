5e92e6a4abd80753797be7ae1087293c0d249df4
Guillermo Padilla <gcpadilla@gmail.com>
2020-07-23 17:11:24 -0300

    cards

--- frontend/src/components/OfertaInicioUser.jsx
+++ frontend/src/components/OfertaInicioUser.jsx
@@ -4,25 +4,27 @@ import CardPostulation from "./CardPostulation";
 const OfertasInicioUser = (props) => {
   return (
     <>
-        <div className="OfertasDeTrabajo d-flex flex-column my-5">
+        <div className="d-flex justify-content-md-start justify-content-sm-start  flex-column my-5">
           <div className="titulares d-flex d-flex justify-content-center justify-content-md-around">
-            <h3 className="titulos col-10">Ofertas de trabajo</h3>
+            <h3 className="postulationInicio titulos col-10">Ofertas de trabajo</h3>
           </div>
           
-          <div className="container">
-            <div className="d-flex flex-wrap justify-content-sm-center justify-content-md-start">
-              {props.datauser.map((a, i) => {
-                return (
-                  <CardPostulation
-                    all={a}
-                    titulo={a.title}
-                    descripcion={a.summary}
-                    vacantes={a.quota}
-                    key={i}
-                    get={props.get}
-                  />
-                );
-              })}
+          <div className="d-flex flex-column flex-wrap align-items-center my-5 ">
+            <div className="container">
+              <div className="d-flex flex-wrap justify-content-sm-center justify-content-md-start">
+                {props.datauser.map((a, i) => {
+                  return (
+                    <CardPostulation
+                      all={a}
+                      titulo={a.title}
+                      descripcion={a.summary}
+                      vacantes={a.quota}
+                      key={i}
+                      get={props.get}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
