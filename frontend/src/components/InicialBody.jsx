115d5f74e1f169e651b14bb57fba619abbda0949
Guillermo Padilla <gcpadilla@gmail.com>
2020-08-12 18:50:30 -0300

    fixed

--- frontend/src/components/InicialBody.jsx
+++ frontend/src/components/InicialBody.jsx
@@ -4,9 +4,9 @@ import Search from "./Search";
 
 const InicialBody = () => {
   return (
-    <div className="homeInicial d-flex flex-column align-items-center justify-content-around">
+    <div className="homeInicial d-flex flex-column align-items-center justify-content-start">
       <img className="logoPortada" src={logo} alt="Logo" />
-      <div className="text-white text-center m-n5">
+      <div className="text-white text-center mt-5">
         <h3>El paso a un</h3>
         <h3 className="infoInicial">nuevo trabajo</h3>
         <h3>empieza aquí</h3>
